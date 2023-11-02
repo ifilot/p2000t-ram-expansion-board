d92414e2427b63e16ad5116dc32d22b6d09bf4dc
ifilot <ivo@ivofilot.nl>
2023-11-02 21:47:22 +0100

    Revising PCB and updating code

--- ramtester/main.c
+++ ramtester/main.c
@@ -26,40 +26,47 @@ int main(void) {
     uint8_t testflag_highmem = 0x00;
     for(uint16_t j=0; j<0x4000; j++) {
         if(highmem[j] != (uint8_t)(j & 0xFF)) {
-            testflag_highmem = 0xFF;
+            testflag_highmem++;
         }
     }
 
-    sprintf(&vidmem[0x00], "High memory: %s", testflag_highmem == 0x00 ? "PASS" : "FAIL");
+    sprintf(&vidmem[0x00], "High memory: %s (%04X)", testflag_highmem == 0 ? "PASS" : "FAIL", testflag_highmem);
 
     // loop over the banks
-    for(uint8_t i=0; i<2; i++) {
+    for(uint8_t i=0; i<6; i++) {
 
         z80_outp(0x94, i);  // set bank
 
         // write value to bank
         for(uint16_t j=0; j<0x2000; j++) {
-            bankmem[j] = ((uint8_t)(j & 0xFF) - i);
+            bankmem[j] = i;
         }
 
         sprintf(&vidmem[0x50*(i+2)], "Writing values to bank: %02i", i);
     }
 
     // loop over the banks
-    for(uint8_t i=0; i<2; i++) {
+    for(uint8_t i=0; i<6; i++) {
 
+        uint16_t firsterror = 0;
+        uint16_t lasterror = 0;
         z80_outp(0x94, i);  // set bank
 
         uint16_t testflag = 0;
 
         // assert value for bank
         for(uint16_t j=0; j<0x2000; j++) {
-            if(bankmem[j] != ((uint8_t)(j & 0xFF) - i)) {
+            if(bankmem[j] != i) {
                 testflag++;
+                if(testflag == 1) {
+                    firsterror = j;
+                }
+                lasterror = j;
             }
         }
 
-        sprintf(&vidmem[0x50*(i+9)], "Bank test: %02i - %s (%04X)", i, testflag == 0x00 ? "PASS" : "FAIL", testflag);
+        sprintf(&vidmem[0x50*(i+9)], "Bank test: %02i - %s (%04X / %04X / %04X)",
+            i, testflag == 0 ? "PASS" : "FAIL", testflag, firsterror, lasterror);
     }
 
     return 0;
